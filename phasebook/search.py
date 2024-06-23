9fd520b51b4cfede5e59c8190d3cd761cb6d64b7
Tengggg11 <villarubiapatrick@gmail.com>
2024-06-23 11:00:39 +0800

    Match optimized, search, and Bonus challenge

--- phasebook/search.py
+++ phasebook/search.py
@@ -26,5 +26,64 @@ def search_users(args):
     """
 
     # Implement search here!
+    results = []
+    if args == {}:
+        return USERS
 
-    return USERS
+    # for user in USERS:
+
+    #     # search for id
+    #     if "id" in args:
+    #         if user['id'] == args['id']:
+    #             results.append(user)
+    #             continue
+
+    #     # partial search for name
+    #     if "name" in args:
+    #         # if user["name"] in USERS 
+    #         if args["name"].lower() in user["name"].lower():
+    #             results.append(user)
+    #             continue
+        
+    #     # search for age (+-1) 
+    #     if "age" in args:
+    #         if (user['age'] >= int(args['age'])-1) and (user['age'] <= int(args['age'])+1):
+    #             results.append(user)
+    #             continue
+        
+    #     if "occupation" in args:
+    #         if args["occupation"].lower() in user["occupation"].lower():
+    #             results.append(user)
+    #             continue
+
+    # return results
+
+    # tempUsers = USERS
+
+    
+    # BONUS CHALLENGE
+    seen_ids = []
+    for arg in args:
+        for user in USERS:
+            if arg == "id":
+                if user["id"] == args["id"] and user["id"] not in seen_ids:
+                    results.append(user)
+                    seen_ids.append(user["id"])
+                    continue
+            elif arg == "age":
+                if (user["age"] >= int(args["age"]) - 1) and (user["age"] <= int(args["age"]) + 1) and user["id"] not in seen_ids:
+                    results.append(user)
+                    seen_ids.append(user["id"])
+                    continue
+            elif arg == "name":
+                if args["name"].lower() in user["name"].lower() and user["id"] not in seen_ids:
+                    results.append(user)
+                    seen_ids.append(user["id"])
+                    continue
+            elif arg == "occupation":
+                if args["occupation"].lower() in user["occupation"].lower() and user["id"] not in seen_ids:
+                    results.append(user)
+                    seen_ids.append(user["id"])
+                    continue
+
+    return results
